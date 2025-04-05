f363f461cfda1cdefcaacd568aac7c570f584786
Alvin-Tulud <alvintulud@gmail.com>
2025-04-05 11:55:28 -0700

    finished task adder script

--- TaskGachi/Assets/Scripts/TasksManagement/TaskAdder.cs
+++ TaskGachi/Assets/Scripts/TasksManagement/TaskAdder.cs
@@ -1,35 +1,56 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TaskAdder : MonoBehaviour
 {
-    public TaskObject object_to_make;
-
     public TaskManager task_manager;
 
-    //creates a task given and name and days hours and minutes needed
-    public void CreateTask(string TaskName, int days, int hours, int minutes)
+    public TMP_InputField ItaskName;
+    public TMP_Dropdown DtaskDays;
+    public TMP_Dropdown DtaskHours;
+    public TMP_Dropdown DtaskMinutes;
+
+    public void AddTask()
     {
+
+    }
+
+    //creates a task given and name and days hours and minutes needed
+    public void CreateTask()
+    {
+        TaskObject TObj = ScriptableObject.CreateInstance<TaskObject>();
+        string TaskName;
+        int days, hours, minutes;
+
+
+        TaskName = ItaskName.text;
+        days = DtaskDays.value;
+        hours = DtaskHours.value;
+        minutes = DtaskMinutes.value;
+
+
         //create new task object
-        object_to_make = new TaskObject();
+        TObj = new TaskObject();
         //set name
-        object_to_make.Task_Name = TaskName;
+        TObj.Task_Name = TaskName;
 
         //create timespan for time inputted
         TimeSpan timeset = new TimeSpan(days, hours, minutes, 0);
         //set that to total time given on object
-        object_to_make.Time_TotalSet = timeset;
+        TObj.Time_TotalSet = timeset;
 
         //get the future time the task will be done at
         DateTime FutureTime = CurrentTIme.getCurrentTime() + timeset;
         //get value of how much time is left
-        object_to_make.Time_Deadline = FutureTime;
+        TObj.Time_Deadline = FutureTime;
 
         //set completion states to false
-        object_to_make.Task_Completed = false;
-        object_to_make.Task_Failed = false;
+        TObj.Task_Completed = false;
+        TObj.Task_Failed = false;
 
         //send task object to taskmanager script
-        task_manager.addTask(object_to_make);
+        task_manager.addTask(TObj);
     }
 }
