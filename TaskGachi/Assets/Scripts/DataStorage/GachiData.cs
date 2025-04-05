dea419615e3759afd0607e100e3d06853ce347c7
Alvin-Tulud <alvintulud@gmail.com>
2025-04-04 17:35:35 -0700

    data directory stuff start

---
+++ TaskGachi/Assets/Scripts/DataStorage/GachiData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class LoadGachiData : MonoBehaviour
+{
+    string filepath = Application.persistentDataPath + "/" + "GachiData";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TaskGachi/Assets/Scripts/DataStorage/TaskData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using static UnityEditor.U2D.ScriptablePacker;
+
+public class LoadTaskData : MonoBehaviour
+{
+    string filepath = Application.persistentDataPath + "/" + "TaskData";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
