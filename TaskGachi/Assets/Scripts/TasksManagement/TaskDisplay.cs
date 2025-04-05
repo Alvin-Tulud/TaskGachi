8199939bd982ffe30d3aff1b5ffd360848ad4ced
LePuncake321 <lucky713ph@yahoo.com>
2025-04-05 16:03:12 -0700

    Tasks DO spawn, just not with image

--- TaskGachi/Assets/Scripts/TasksManagement/TaskDisplay.cs
+++ TaskGachi/Assets/Scripts/TasksManagement/TaskDisplay.cs
@@ -1,29 +1,33 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Hierarchy;
 
 public class TaskDisplay : MonoBehaviour
 {
     // Set to the game object representing the tasks
     public GameObject taskObject;
+    // Where the Tasks will appear
+    public GameObject canvas;
     // To access the tasks
     private TaskManager manae;
 
     private Transform firstTaskLocation;
-    private float offset = 4.0F;
+    private float offset = 20.0F;
     private int numberOfTasks;
 
     public void showTasks() {
         // 1. Spawn all tasks currently stored in the TaskManager
-        List<TaskObject> taskList = manae.getList();
-        numberOfTasks = taskList.Count;
+        //List<TaskObject> taskList = manae.getList();
+        //numberOfTasks = taskList.Count;
 
-        for (int i = 0; i < numberOfTasks; i++) {
-            Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y + (i * offset));
+        for (int i = 0; i < 10; i++) {
+            Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y - (i * offset));
             
             GameObject spawnedObject = Instantiate(taskObject, spawnPosition, Quaternion.identity);
             
-            spawnedObject.transform.parent = transform;
+            spawnedObject.transform.SetParent(canvas.transform);
+            spawnedObject.SetActive(true);
 
         }
     }
